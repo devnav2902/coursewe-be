a99be28f9eac77e2e355e93f78bd14e491930f54
Nguyen Anh Vu <63908788+devnav2902@users.noreply.github.com>
2022-03-01 07:37:40 +0700

    chore: add course & login api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// COURSE
+Route::get('course', [CourseController::class, 'getCourse']);
+
+// USER
+Route::post('user/login', [UserController::class, 'login']);
