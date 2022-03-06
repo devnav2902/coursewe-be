38f66460418b2cc14f1b907d6a01149de2984e3a
Nguyen Anh Vu <63908788+devnav2902@users.noreply.github.com>
2022-03-06 08:11:42 +0700

    chore: add symbolic links(profile_picture)

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
@@ -68,6 +68,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('profile_picture') => storage_path('app/profile_picture'),
     ],
 
 ];
