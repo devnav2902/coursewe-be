af9543f19884df3d002084b752e2b269838985b1
Nguyen Anh Vu <63908788+devnav2902@users.noreply.github.com>
2022-03-06 11:21:15 +0700

    chore: add purchase history api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\PurchaseHistoryController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -26,4 +27,5 @@ Route::get('/user', [UserController::class, 'getCurrentUser']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user/logout', [UserController::class, 'logout']);
+    Route::get('/purchase/history', [PurchaseHistoryController::class, 'purchaseHistory']);
 });
