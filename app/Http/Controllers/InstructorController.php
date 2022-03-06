8c0f4997268a0ab398e9829fd84a1a94d479c58e
Nguyen Anh Vu <63908788+devnav2902@users.noreply.github.com>
2022-03-06 17:18:21 +0700

    chore: create instructor controller

---
+++ app/Http/Controllers/InstructorController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InstructorController extends Controller
+{
+    public function index($slug)
+    {
+        $author = User::with('bio')
+            ->where('slug', $slug)
+            ->first(['id', 'avatar', 'fullname']);
+
+        if (!$author) return abort(404);
+
+        $course = Course::where('author_id', $author->id)
+            ->where('isPublished', 1)
+            ->withCount('course_bill')
+            ->withCount('rating');
+
+        $queryTotalStudents = clone $course;
+        $totalStudents = $queryTotalStudents
+            ->get(['id'])
+            ->sum('course_bill_count');
+
+        $queryTotalCourses = clone $course;
+        $totalCourses = $queryTotalCourses->get(['id'])->count();
+
+        $queryTotalReviews = clone $course;
+        $totalReviews = $queryTotalReviews->whereHas('rating', function ($q) {
+            $q->where('content', '<>', '');
+        })
+            ->get(['id'])
+            ->sum('rating_count');
+
+        $courses = $course->orderBy('created_at', 'desc')
+            ->withAvg('rating', 'rating')
+            ->paginate(
+                6,
+                ['title', 'id', 'author_id', 'created_at', 'price', 'slug']
+            );
+
+        return view('pages.instructor-bio', compact(['author', 'courses', 'totalStudents', 'totalReviews', 'totalCourses']));
+    }
+}
