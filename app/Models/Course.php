7ed2c0c7ef644a7a0b2abf2e410a336b6924e0a1
nguyynavu <nguyenanhvu.work@gmail.com>
2022-03-09 08:01:29 +0700

    Add course relationships

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -10,6 +10,18 @@ class Course extends Model
     use HasFactory;
     protected $table = 'course';
     protected $fillable = ['author_id', 'title', 'description', 'slug', 'thumbnail', 'video_demo', 'isPublished', 'price', 'discount', 'submit_for_review'];
+    protected $with =
+    [
+        'author',
+        'rating',
+        'course_bill',
+        // 'price'
+    ];
+
+    function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 
     function coupon()
     {
@@ -21,4 +33,23 @@ class Course extends Model
     {
         return $this->hasMany(Rating::class, 'course_id');
     }
+
+    function course_bill()
+    {
+        return $this->hasMany(CourseBill::class, 'course_id');
+    }
+
+    function section()
+    {
+        return $this->hasMany(Section::class, 'course_id')->orderBy('order', 'asc');
+    }
+
+    function lecture()
+    {
+        return $this->hasManyThrough(Lecture::class, Section::class, 'course_id', 'section_id');
+    }
+    // function price()
+    // {
+    //     return $this->belongsTo(Price::class, 'price_id');
+    // }
 }
