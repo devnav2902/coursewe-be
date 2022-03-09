8fdbd2ba6c6878ddea22b2341219aa22fafb2f26
nguyynavu <nguyenanhvu.work@gmail.com>
2022-03-09 08:03:31 +0700

    Add api: get course by slug & instructor profile

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\InstructorController;
 use App\Http\Controllers\PurchaseHistoryController;
 use App\Http\Controllers\UserController;
 
@@ -19,11 +20,13 @@ use App\Http\Controllers\UserController;
 
 // COURSE
 Route::get('/course', [CourseController::class, 'getCourse']);
+Route::post('/course', [CourseController::class, 'getCourseBySlug']);
 
 // USER
 Route::post('/user/login', [UserController::class, 'login']);
 Route::post('/user/sign-up', [UserController::class, 'signUp']);
 Route::get('/user', [UserController::class, 'getCurrentUser']);
+Route::get('/instructor/profile/{slug}', [InstructorController::class, 'profile']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user/logout', [UserController::class, 'logout']);
