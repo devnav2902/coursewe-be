a2d6f990d2c6f870cec5c57c8e52df44e024c0a3
Nobitu <sam@google.com>
2022-03-29 10:37:11 +0700

    chore: model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,8 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class, 'role_id');
     }
+    public function course()
+    {
+        return $this->hasMany(Course::class, 'author_id');
+    }
 }
