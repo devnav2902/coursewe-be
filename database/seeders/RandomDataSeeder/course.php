d95dbc10a69ddc2711fd5786ece4fd427a83f0b3
nguyynavu <nguyenanhvu.work@gmail.com>
2022-04-08 08:44:57 +0700

    Test data

---
+++ database/seeders/RandomDataSeeder/course.php
@@ -0,0 +1,65 @@
+<?php
+function course()
+{
+  [
+    'ngoai-ngu' => [
+      'title' => [
+        'Tiếng Trung cơ bản 1',
+        'Tiếng Anh cho người mất gốc',
+        'Luyện thi TOEIC 500+ từ con số 0',
+        'IELTS Writing 6.0 cho người mới',
+        'Tiếng Anh cho người mới bắt đầu',
+        'Tạo dựng mối quan hệ bằng tiếng Anh',
+        'IELTS Academic - Module Speaking',
+        'Tiếng Trung cho doanh nhân',
+        'IELTS through BBC',
+        'Học tiếng Nhật - Hướng dẫn cách giải và dịch đề thi JLPT N5',
+        'Tiếng Anh giao tiếp với giáo viên bản ngữ về chủ đề âm nhạc và điện ảnh',
+        'Khóa học tiếng Nhật cơ bản',
+        'Tiếng Hàn BIGMAXX Online - Trung cấp 2',
+        'Tiếng Hàn TOPIK 3',
+        'Tự học tiếng Hàn - sơ cấp 2',
+        'Mỗi ngày một ngữ pháp tiếng Hàn'
+      ],
+
+      'thumbnail' => [
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F191ab4fd-9c62-4494-b209-51f86a3924d3%252Fproduct%252F613c5f5ce818b400255e3bd5&w=1920&q=50',
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F191ab4fd-9c62-4494-b209-51f86a3924d3%252Fproduct%252F60c1c0a0e1383a0025b58b4d&w=1920&q=50',
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F191ab4fd-9c62-4494-b209-51f86a3924d3%252Fproduct%252F613ccef4e818b400255e407b&w=1920&q=50',
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F5a7a63749f7bfc459700033e%252Fproduct%252F6004ef9d44d203002598c209&w=1920&q=50',
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F99e8262a-7b95-4050-a1a0-d6977c9d98ed%252Fproduct%252F60052a4c132cb60024b0288d&w=1920&q=50',
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F99e8262a-7b95-4050-a1a0-d6977c9d98ed%252Fproduct%252F600511b444d203002598c298&w=1920&q=50',
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F191ab4fd-9c62-4494-b209-51f86a3924d3%252Fproduct%252F6076602d31b9ae0024d5da96&w=1920&q=50',
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F5a7a63749f7bfc459700033e%252Fproduct%252F60010a7844d203002598bde8&w=1920&q=50',
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F5a7a63749f7bfc459700033e%252Fproduct%252F600009fe44d203002598bc1f&w=1920&q=50',
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F99e8262a-7b95-4050-a1a0-d6977c9d98ed%252Fproduct%252F6001522d44d203002598c01d&w=1920&q=50',
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F5a7a63749f7bfc459700033e%252Fproduct%252F6001826244d203002598c16c&w=1920&q=50',
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F5a7a63749f7bfc459700033e%252Fproduct%252F600008b344d203002598bc0f&w=1920&q=50'
+      ]
+    ],
+    'phat-trien-ban-than' => [
+      'title' => [
+        'Kỹ năng giao tiếp trong công sở',
+        'Bí quyết giao tiếp để thành công',
+        'Làm chủ kỹ năng giao tiếp trong vòng 7 ngày',
+        'Kỹ năng giao tiếp qua các tình huống',
+        'Tư duy phản biện - giải quyết đến tận cùng vấn đề',
+        'Khóa học ý thức tư duy và liên tưởng',
+        'Làm chủ tư duy thay đổi vận mệnh',
+        'Học làm lãnh đạo',
+        'Bậc thầy lãnh đạo',
+        'Nhà Lãnh đạo phải biết đào tạo',
+        'Học để trở thành lãnh đạo cấp cao',
+        'Quản trị tài chính cá nhân hiệu quả',
+        'Khóa học quản lý tài chính cá nhân một cách hiệu quả'
+      ],
+
+      'thumbnail' => [
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F191ab4fd-9c62-4494-b209-51f86a3924d3%252Fproduct%252F6076989731b9ae0024d5dba8&w=1920&q=50',
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F191ab4fd-9c62-4494-b209-51f86a3924d3%252Fproduct%252F60a24305d12b64002515b0be&w=1920&q=50',
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F191ab4fd-9c62-4494-b209-51f86a3924d3%252Fproduct%252F60754a0b31b9ae0024d5d6f8&w=1920&q=50',
+        'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F99e8262a-7b95-4050-a1a0-d6977c9d98ed%252Fproduct%252F5ffff55044d203002598bbc7&w=1920&q=50', 'https://edumall.vn/_next/image?url=%2Fapi%2Fimageproxy%3Furl%3Dhttps%253A%252F%252Fcdn2.topica.vn%252F191ab4fd-9c62-4494-b209-51f86a3924d3%252Fproduct%252F609badf9d12b640025159f94&w=1920&q=50'
+      ]
+    ],
+  ];
+}
