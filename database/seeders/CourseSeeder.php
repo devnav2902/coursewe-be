4b2e4891761b54344b77dce099d8b51f036fca9b
nguyynavu <nguyenanhvu.work@gmail.com>
2022-04-08 08:42:41 +0700

    Update course seeder

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -21,7 +21,125 @@ class CourseSeeder extends Seeder
     public function run()
     {
         $thumbnail = thumbnail();
+
         $courses = [
+            [
+                'title' => 'Tiếng Trung cơ bản 1',
+                'category' => ['tieng-trung']
+            ],
+            [
+                'title' => 'Tiếng Anh cho người mất gốc',
+                'category' => ['tieng-anh']
+            ],
+            [
+                'title' => 'Luyện thi TOEIC 500+ từ con số 0',
+                'category' => ['toeic']
+            ],
+            [
+                'title' => 'IELTS Writing 6.0 cho người mới',
+                'category' => ['ielts']
+            ],
+            [
+                'title' => 'Tiếng Anh cho người mới bắt đầu',
+                'category' => ['tieng-anh']
+            ],
+            [
+                'title' => 'Tạo dựng mối quan hệ bằng tiếng Anh',
+                'category' => ['tieng-anh']
+            ],
+            [
+                'title' => 'IELTS Academic - Module Speaking',
+                'category' => ['ielts']
+            ],
+            [
+                'title' => 'Tiếng Trung cho doanh nhân',
+                'category' => ['tieng-trung']
+            ],
+            [
+                'title' => 'IELTS through BBC',
+                'category' => ['ielts']
+            ],
+            [
+                'title' => 'Học tiếng Nhật - Hướng dẫn cách giải và dịch đề thi JLPT N5',
+                'category' => ['tieng-nhat']
+            ],
+            [
+                'title' =>
+                'Tiếng Anh giao tiếp với giáo viên bản ngữ về chủ đề âm nhạc và điện ảnh',
+                'category' => ['tieng-anh']
+            ],
+            [
+                'title' => 'Khóa học tiếng Nhật cơ bản',
+                'category' => ['tieng-nhat']
+            ],
+            [
+                'title' => 'Tiếng Hàn BIGMAXX Online - Trung cấp 2',
+                'category' => ['tieng-han']
+            ],
+            [
+                'title' => 'Tiếng Hàn TOPIK 3',
+                'category' => ['tieng-han']
+            ],
+            [
+                'title' => 'Tự học tiếng Hàn - sơ cấp 2',
+                'category' => ['tieng-han']
+            ],
+            [
+                'title' => 'Mỗi ngày một ngữ pháp tiếng Hàn',
+                'category' => ['tieng-han']
+            ],
+            [
+                'title' => 'Kỹ năng giao tiếp trong công sở',
+                'category' => ['giao-tiep']
+            ],
+            [
+                'title' => 'Bí quyết giao tiếp để thành công',
+                'category' => ['giao-tiep']
+            ],
+            [
+                'title' => 'Làm chủ kỹ năng giao tiếp trong vòng 7 ngày',
+                'category' => ['giao-tiep']
+            ],
+            [
+                'title' => 'Kỹ năng giao tiếp qua các tình huống',
+                'category' => ['giao-tiep']
+            ],
+            [
+                'title' => 'Tư duy phản biện - giải quyết đến tận cùng vấn đề',
+                'category' => ['tu-duy']
+            ],
+            [
+                'title' => 'Khóa học ý thức tư duy và liên tưởng',
+                'category' => ['tu-duy']
+            ],
+            [
+                'title' => 'Làm chủ tư duy thay đổi vận mệnh',
+                'category' => ['tu-duy']
+            ],
+            [
+                'title' => 'Học làm lãnh đạo',
+                'category' => ['lanh-dao']
+            ],
+            [
+                'title' => 'Bậc thầy lãnh đạo',
+                'category' => ['lanh-dao']
+            ],
+            [
+                'title' => 'Nhà Lãnh đạo phải biết đào tạo',
+                'category' => ['lanh-dao']
+            ],
+            [
+                'title' => 'Học để trở thành lãnh đạo cấp cao',
+                'category' => ['lanh-dao']
+            ],
+            [
+                'title' => 'Quản trị tài chính cá nhân hiệu quả',
+                'category' => ['ca-nhan']
+            ],
+            [
+                'title' => 'Khóa học quản lý tài chính cá nhân một cách hiệu quả',
+                'category' => ['ca-nhan']
+            ],
             [
                 'title' => 'KHÓA HỌC ANGULAR & TYPESCRIPT (FRONT END)',
                 'category' => ['angularjs', 'typescript'],
@@ -97,10 +215,11 @@ class CourseSeeder extends Seeder
                 'lecture' => 'Một số nội dung cần nắm'
             ],
         ];
-        $course_subtitle = "Good course for everyone";
+        $course_subtitle = "Khóa học dành cho những bạn mới bắt đầu";
         $description = "<div class='study-benefit__list'><div class='study-benefit__item'><i class='fa fa-check icon'></i>Lượng kiến thức khổng lồ về Javascript được phân bổ hợp lý theo từng video một</div><div class='study-benefit__item'><i class='fa fa-check icon'></i>Có kiến thức nền tảng tốt về Javascript để có thể học cao lên sau này</div><div class='study-benefit__item'><i class='fa fa-check icon'></i>Làm được những chức năng, hiệu ứng trên web từ a-z</div><div class='study-benefit__item'><i class='fa fa-check icon'></i>Biết thế nào là API, xử lý dữ liệu, xử lý logic, tư duy giải quyết vấn đề tốt</div><div class='study-benefit__item'><i class='fa fa-check icon'></i>Mỗi video giải thích kỹ càng và chi tiết, cũng như thời lượng video ngắn giúp bạn học hiệu quả hơn</div><div class='study-benefit__item'><i class='fa fa-check icon'></i>Được chia sẻ nhiều tips, tricks, các nguồn học bổ ích để cải thiện trình độ</div><div class='study-benefit__item'><i class='fa fa-check icon'></i>Và rất nhiều kiến thức khác đang chờ đợi các bạn trong khoá học này</div></div>";
 
-        foreach ($courses as $course) {
+        for ($i = 0; $i < 100; $i++) {
+            $course = $courses[random_int(1, count($courses) - 1)];
             $author_id = random_int(1, 10);
             $levelId = random_int(0, 3);
             $id = db::table('course')->insertGetId(
@@ -111,7 +230,7 @@ class CourseSeeder extends Seeder
                     'price_id' => random_int(1, 19),
                     'title' => $course['title'],
                     'description' => $description,
-                    'slug' => Str::slug($course['title']),
+                    'slug' => Str::slug($course['title'] . '-' . random_int(1, 1000)),
                     'video_demo' => 'lesson/test.mp4',
                     'isPublished' => 1,
                     'thumbnail' => $thumbnail[random_int(1, count($thumbnail) - 1)]
@@ -171,5 +290,80 @@ class CourseSeeder extends Seeder
                 ]);
             }
         }
+
+        // foreach ($courses as $course) {
+        //     for ($i = 0; $i < 5; $i++) {
+
+        //         $author_id = random_int(1, 10);
+        //         $levelId = random_int(0, 3);
+        //         $id = db::table('course')->insertGetId(
+        //             [
+        //                 'instructional_level_id' => $levelId,
+        //                 'subtitle' => $course_subtitle,
+        //                 'author_id' => $author_id,
+        //                 'price_id' => random_int(1, 19),
+        //                 'title' => $course['title'],
+        //                 'description' => $description,
+        //                 'slug' => Str::slug($course['title'] . '-' . random_int(1, 1000)),
+        //                 'video_demo' => 'lesson/test.mp4',
+        //                 'isPublished' => 1,
+        //                 'thumbnail' => $thumbnail[random_int(1, count($thumbnail) - 1)]
+        //             ]
+        //         );
+
+        //         DB::table('bio')->insert(
+        //             [
+        //                 'headline' =>
+        //                 'Học và làm việc tại trường Giao Thông Vận Tải TP.Hồ Chí Minh',
+        //                 'bio' => ' Mình là một Frontend Developer, ngoài ra mình còn viết blog và làm youtube nữa. Mình thích chia sẻ kiến thức tới cộng đồng, giúp đỡ các bạn theo ngành này có thể học hỏi nâng cao trình độ hơn mỗi ngày. Hi vọng khoá học của mình sẽ giúp các bạn cải thiện được trình độ nhiều nhất có thể.',
+        //                 'youtube' => 'https://www.youtube.com/channel/UCLphTurxkwnUZpOAPXSjw0g',
+        //                 'facebook' => 'https://www.facebook.com',
+        //                 'linkedin' => 'https://www.linkedin.com',
+        //                 'user_id' => $author_id
+        //             ]
+        //         );
+
+        //         foreach ($course['category'] as $cat) {
+        //             $cat_id = db::table('categories')
+        //                 ->where('slug', 'LIKE', '%' . $cat . '%')
+        //                 ->first('category_id');
+
+        //             if ($cat_id) {
+        //                 db::table('categories_course')
+        //                     ->insert(
+        //                         [
+        //                             'course_id' => $id,
+        //                             'category_id' => $cat_id->category_id
+        //                         ]
+        //                     );
+        //             }
+        //         }
+
+        //         foreach ($sections as $key => $section) {
+
+        //             $category = '';
+        //             foreach ($course['category'] as $key => $value) {
+        //                 $key !== count($course['category']) - 1
+        //                     ? $category .= $value . ", "
+        //                     : $category .= $value;
+        //             }
+        //             $sec_id = db::table('sections')->insertGetId(
+        //                 [
+        //                     'course_id' => $id,
+        //                     'title' => $section['title'] . " " . $category,
+        //                     'order' => $key + 1
+        //                 ]
+        //             );
+
+
+        //             db::table('lectures')->insertGetId([
+        //                 'section_id' => $sec_id,
+        //                 'order' => $key + 1,
+        //                 'title' => $section['lecture'],
+        //                 'src' => 'lesson/test.mp4'
+        //             ]);
+        //         }
+        //     }
+        // }
     }
 }
