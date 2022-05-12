c153a04e4ec5fd7722bf3bb485d0ac983d6f4d08
nguyynavu <nguyenanhvu.work@gmail.com>
2022-05-12 08:30:24 +0700

    Create schedule: check coupon

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\HandleCouponsIfExpired;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,7 +16,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(HandleCouponsIfExpired::class)
+            ->everyMinute()
+            ->sendOutputTo(storage_path('logs/HandleCouponsIfExpired.log'));
+        // $schedule->command('inspire')->everyMinute();
     }
 
     /**
@@ -25,7 +29,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
