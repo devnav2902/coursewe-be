6ebae3c24c624676f4cd64e6e665acaed5be47d4
nguyynavu <nguyenanhvu.work@gmail.com>
2022-05-12 08:31:06 +0700

    Update controllers

--- app/Http/Controllers/InstructionalLevelController.php
+++ app/Http/Controllers/InstructionalLevelController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\InstructionalLevel;
-use Illuminate\Http\Request;
 
 class InstructionalLevelController extends Controller
 {
@@ -12,29 +11,5 @@ class InstructionalLevelController extends Controller
         $instructionalLevel = InstructionalLevel::get();
 
         return response()->json(compact(['instructionalLevel']));
-    }
-
-    function amountCoursesByInstructionalLevel($slug)
-    {
-        $helperController = new HelperController();
-        $coursesBySlug = $helperController->getCoursesByCategorySlug($slug, false);
-
-        $levels = InstructionalLevel::get();
-        $levelInCourses = $coursesBySlug->pluck('instructional_level');
-        $countCoursesByLevel = $levelInCourses->countBy('level');
-
-        return $levels->transform(function ($level) use ($countCoursesByLevel) {
-            $name = $level['level'];
-            $amount = 0;
-
-            $data = ['name' => $name, 'id' => $level['id'], 'amount' => $amount];
-
-            if (isset($countCoursesByLevel[$name])) {
-                $amount = $countCoursesByLevel[$name];
-                $data['amount'] = $amount;
-            }
-
-            return $data;
-        });
     }
 }
