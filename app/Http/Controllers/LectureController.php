95779cb834cdb418b2a03d449e931c32f9ac81da
nguyynavu <nguyenanhvu.work@gmail.com>
2022-05-24 16:07:45 +0700

    Update controllers: Lecture, Section, PublishCourse

--- app/Http/Controllers/LectureController.php
+++ app/Http/Controllers/LectureController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use App\Models\Lecture;
+use App\Models\Section;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,6 +15,69 @@ class LectureController extends Controller
         $lecture = Lecture::find($lectureId);
 
         return response(['lecture' => $lecture]);
+    }
+
+    function createLecture(Request $request)
+    {
+        $request->validate([
+            'courseId' => 'required|alpha_num',
+            'sectionId' => 'required',
+            'title' => 'required|string'
+        ]);
+
+        $sectionId = $request->input('sectionId');
+        $courseId = $request->input('courseId');
+        $title = $request->input('title');
+
+        $existSection = Course::where([
+            ['id', $courseId],
+            ['author_id', Auth::user()->id]
+        ])
+            ->select('id', 'author_id')
+            ->whereHas('section', function ($q) use ($sectionId) {
+                $q->where('id', $sectionId);
+            })
+            ->first();
+
+        if ($existSection) {
+            $maxOrder = Lecture::whereHas('section', fn ($q) => $q->where('id', $sectionId))->max('order') ?? 0;
+            $result = Lecture::create(['title' => $title, 'section_id' => $sectionId, 'order' => $maxOrder + 1]);
+
+            return response(['success' => true, 'lecture' => $result]);
+        }
+
+        return response(['message' => 'Không thể lưu nội dung này!'], 400);
+    }
+
+    function updateTitle(Request $request)
+    {
+        $request->validate([
+            'courseId' => 'required|alpha_num',
+            'lectureId' => 'required',
+            'title' => 'required|string'
+        ]);
+
+        $lectureId = $request->input('lectureId');
+        $courseId = $request->input('courseId');
+        $title = $request->input('title');
+
+        $existLecture = Course::where([
+            ['id', $courseId],
+            ['author_id', Auth::user()->id]
+        ])
+            ->select('id', 'author_id')
+            ->whereHas('lecture', function ($q) use ($lectureId) {
+                $q->where('lectures.id', $lectureId);
+            })
+            ->first();
+
+        if ($existLecture) {
+            Lecture::where('id', $lectureId)->update(['title' => $title]);
+
+            return response(['success' => true]);
+        }
+
+        return response(['message' => 'Không tồn tại bài giảng này!'], 400);
     }
 
     function upload(Request $request)
@@ -35,7 +99,7 @@ class LectureController extends Controller
             $helper = new HelperController();
             // $filesize = $helper->niceBytes($file->getSize());
             $filename = 'user' . Auth::user()->id . '_lectures_' . time() . '.' . $file->getClientOriginalExtension();
-            $path = $file->storeAs('lectures', $filename);
+            $path = $file->storeAs('lessons', $filename);
 
             $dataDuration = $helper->getDuration($path);
 
@@ -71,12 +135,52 @@ class LectureController extends Controller
 
             if ($exist) {
                 Lecture::destroy($lectureId);
-                return response(['success' => true]);
+                return response(['success' => true, 'deleted' => $lectureId]);
             }
 
             return response(['success' => false, 'message' => 'lecture not exist']);
         } catch (\Throwable $th) {
             return response(['success' => false], 400);
         }
+    }
+
+    function reorder(Request $request, $sectionId, $courseId)
+    {
+        $request->validate([
+            'data' => 'required|array',
+            'data.*.id' => 'required',
+            'data.*.order' => 'required|numeric',
+        ]);
+
+        $lectureReordered = $request->input('data');
+
+        $countLectures = Lecture::whereHas(
+            'section.course',
+            fn ($q) => $q->setEagerLoads([])
+                ->select('id', 'author_id')
+                ->where([['id', $courseId], ['author_id', Auth::user()->id]])
+        )
+            ->whereHas('section', function ($q) {
+                $q->setEagerLoads([])->select('sections.id')->where('id', 295);
+            })
+            ->select('order', 'id', 'section_id')
+            ->whereIn('lectures.id', collect($lectureReordered)->pluck('id')->toArray())
+            ->count();
+
+        if (count($lectureReordered) === $countLectures) {
+            $data = collect($lectureReordered)
+                ->map(function ($lecture) use ($sectionId) {
+                    $lecture['section_id'] = $sectionId;
+
+                    return $lecture;
+                })
+                ->toArray();
+
+            Lecture::upsert($data, ['section_id', 'id'], ['order']);
+
+            return response('success');
+        }
+
+        return response('error', 400);
     }
 }
